1c82fcacfcd96213a811035eb69d38342b79873a
Suspicious <alex@kaucher.com>
2020-01-13 20:57:12 -0700

    Struggling with ajax calls, will work on later when more time is avaiable

--- Github PDF producer/script.js
+++ Github PDF producer/script.js
@@ -1,37 +1,41 @@
 let fs = require('fs');
 let pdf = require('./node_modules/pdf/lib/pdf').pdf;
-let github = require('octonode')
-/* create the PDF document */
-
-let doc = new pdf();
-doc.text(20, 20, 'hello, I am PDF.');
-doc.text(20, 30, 'i was created using node.js version: ' + process.version);
-doc.text(20, 40, 'i can also be created from the browser');
-
-/* optional - set properties on the document */
-doc.setProperties({
-    title: 'A sample document created by pdf.js',
-    subject: 'PDFs are kinda cool, i guess',		
-    author: 'Marak Squires',
-    keywords: 'pdf.js, javascript, Marak, Marak Squires',
-    creator: 'pdf.js',
-    fillColor: 'rgb(0,0,.9)'
-});
-doc.addPage();
-
-doc.setFontSize(22);
-doc.text(20, 20, 'This is a title');
-
-doc.setFontSize(16);
-doc.text(20, 30, 'This is some normal sized text underneath.');
-
-let fileName = "testFile"+new Date().getSeconds()+".pdf";
-
-fs.writeFile(fileName, doc.output(), function(err, data){//possible extra code, might need to be extracted
-});
-
+let github = require('octonode');
 let client = github.client();
+let data = [];
 
-client.get('/users/pksunkara', {}, function (err, status, body, headers) {
-  console.log(body); //json object
+getData();
+
+async function getData(){
+try {
+
+let userRequestedClient = '/users/'+process.argv[2];
+
+await client.get(userRequestedClient, {}, function (err, status, body, headers) {
+data.push(
+  "image Url= "+(body.avatar_url),
+  "User name= "+(body.login),
+  "Location= "+(body.location),
+  "Github Profile= "+(body.html_url),
+  "Blog= "+(body.blog),
+  "Bio= "+(body.bio),
+  "Public Repos= "+(body.public_repos),
+  "Followers= "+(body.followers),
+  "Following= "+(body.following)
+     );  
+   }); 
+let doc = new pdf();
+for (let i = 0; i<data.length; i++){
+  doc.text(data[i]);
+};
+doc.setProperties({
+    title: 'GitHub Profile',
 });
+
+fs.writeFile("GitHub Profile.pdf", doc.output(), function(err, data){
+  return console.log("error has occured!")
+});}
+catch (err) {
+ console.log("error");
+}
+};
